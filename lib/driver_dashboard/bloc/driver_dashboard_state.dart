be6880e1316da2e4436d835317705bdf5823dd4e
Richard Afoudo <richardafoudo07@gmail.com>
2021-10-22 15:31:03 +0100

    Adjusting drivers dashboard

---
+++ lib/driver_dashboard/bloc/driver_dashboard_state.dart
@@ -0,0 +1,10 @@
+part of 'driver_dashboard_bloc.dart';
+
+abstract class DriverDashboardState extends Equatable {
+  const DriverDashboardState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class DriverDashboardInitial extends DriverDashboardState {}
